16d38eedb40dcfa18bb68012cf75abab56d061f3
jollypeanut <sking@crazyawesome.io>
2015-11-12 17:12:07 -0600

    Split app into various functions. Confirmed over/under to be working at this point

--- hotcold.js
+++ hotcold.js
@@ -1,15 +1,50 @@
+//Hot or Cold Application
 
-var guess = undefined;
-var randomNumber = Math.floor((Math.random() * 100) + 1);
-
-function submit(){
-  if(guess === randomNumber){
-      return 'you got it'
+function startHotCold () { //COMPLETE
+  randomNumberGenerator()
+}
+function randomNumberGenerator () { //COMPLETE
+  randomNumber = Math.floor((Math.random() * 100) + 1);
+  guessNumber();
+}
+function guessNumber () { //COMPLETE
+  guess = undefined;
+  check();
+}
+function check () { //COMPLETE
+  if (guess === undefined) {
+    console.log("Guess A Number.");
   }
-  else  if(guess === undefined){
-      console.log('Guess a number');
+  else if (isNaN (guess) === true) {
+    console.log("That is not a number. Please try again.");
   }
-  else{
-      console.log('nope...guess again');
+  else if (guess > 100) {
+    console.log("That number is over 100. Please try again.");
   }
+  else if (guess < 0) {
+    console.log("That number is below 0. Please try again");
+  }
+  else {
+    if (guess === randomNumber) {
+      isGuess();
+    }
+    else if (guess > randomNumber) {
+      isOverGuess();
+    }
+    else if (guess < randomNumber) {
+      isUnderGuess();
+    }
+  }
+}
+function isOverGuess () {
+  console.log("You are over, guess again.");
+}
+function isUnderGuess () {
+  console.log ("You are under, guess again.");
+}
+function isGuess () {
+  console.log ("You got it!")
+}
+function guessCounter () {
+  //NOT COMPLETED!
 }
