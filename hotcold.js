f0642474638e1e9d52d871f8df4ebbbcfaefe416
jollypeanut <sking@crazyawesome.io>
2015-11-12 18:29:18 -0600

    Moved counter to check function. Added guesses array.

--- hotcold.js
+++ hotcold.js
@@ -1,18 +1,19 @@
 //Hot or Cold Application
 
-function startHotCold () { //COMPLETE
+function startHotCold () {
   randomNumberGenerator()
 }
-function randomNumberGenerator () { //COMPLETE
+function randomNumberGenerator () {
   randomNumber = Math.floor((Math.random() * 100) + 1);
   guessNumber();
 }
-function guessNumber () { //COMPLETE
+function guessNumber () {
   guess = undefined;
-  guessCount = 0
+  guessCount = 0;
+  guesses = [];
   check();
 }
-function check () { //COMPLETE
+function check () {
   if (guess === undefined) {
     console.log("Guess A Number.");
   }
@@ -26,6 +27,8 @@ function check () { //COMPLETE
     console.log("That number is below 0. Please try again");
   }
   else {
+    guessCount ++;
+    guesses.push (guess);
     if (guess === randomNumber) {
       isGuess();
     }
@@ -39,49 +42,38 @@ function check () { //COMPLETE
 }
 function isOverGuess () {
   if (guess < randomNumber + 5) {
-    guessCount ++
-    console.log("Call the fire department, you are on fire! You have guessed " + guessCount + " times.");
+    console.log("Call the fire department, you are on fire! You have guessed " + guessCount + " times." + guesses);
   }
   else if (guess < randomNumber + 10) {
-    guessCount ++
     console.log("Pretty hot, can you feel your skin tingling? You have guessed " + guessCount + " times.");
   }
   else if (guess < randomNumber + 25) {
-    guessCount ++
     console.log("Getting warmer, put still have a couple guesses to go. You have guessed " + guessCount + " times.");
   }
   else if (guess < randomNumber + 50) {
-    guessCount ++
     console.log("Okay, you can take your jacket off but you are still a ways away. You have guessed " + guessCount + " times.");
   }
   else {
-    guessCount ++
     console.log("Better find a jacket, you might get frostbite. You have guessed " + guessCount + " times.");
   }
 }
 function isUnderGuess () {
   if (guess > randomNumber - 5) {
-    guessCount ++
     console.log("Call the fire department, you are on fire! You have guessed " + guessCount + " times.");
   }
   else if (guess > randomNumber - 10) {
-    guessCount ++
     console.log("Pretty hot, can you feel your skin tingling? You have guessed " + guessCount + " times.");
   }
   else if (guess > randomNumber - 25) {
-    guessCount ++
     console.log("Getting warmer, put still have a couple guesses to go. You have guessed " + guessCount + " times.");
   }
   else if (guess > randomNumber - 50) {
-    guessCount ++
     console.log("Okay, you can take your jacket off but you are still a ways away. You have guessed " + guessCount + " times.");
   }
   else {
-    guessCount ++
     console.log("Better find a jacket, you might get frostbite. You have guessed " + guessCount + " times.");
   }
 }
 function isGuess () {
-  guessCount ++
   console.log ("You got it! You took " + guessCount + " guesses.")
 }
